bf3748bfe7be8e45920b4a7683da5baf83332c25
Skwozz <rakalov.s@ya.ru>
2024-04-24 19:57:28 +0500

    Тестовое задание завершено

--- Ecom/Ecom/yasg.py
+++ Ecom/Ecom/yasg.py
@@ -8,6 +8,7 @@ from drf_yasg import openapi
 
 schema_view = get_schema_view(
     openapi.Info(
+
         title="Django API for warhouse",
         default_version='v1',
         description="Test description",
